bcda1a377d1b0ba37ca59632bbaad8cbd83aadf3
Daniel <davilareko@gmail.com>
2023-07-14 17:48:38 +0200

    Add stuff for patching the username creator module

--- LastOnline.plugn.js
+++ LastOnline.plugn.js
@@ -1,7 +1,7 @@
 /**
  * @name LastOnline
  * @description Allows you to see when someone went last online.
- * @author davilarek && _ninelota_
+ * @author davilarek, _ninelota_
  * @version 1.0.0
  */
 
@@ -13,13 +13,36 @@ class LastOnline {
   constructor() {
     this.t = null;
     this.statuses = [];
+    this.patches = [];
   }
 
   getAllUsers() {
     return PresenceStore.getUserIds();
   }
 
+  /**
+   * @param {"after" | "before" | "instead"} patchType
+   * @param {object} moduleToPatch
+   * @param {string} functionName
+   * @param {Function} callback
+   */
+  addPatch(patchType, moduleToPatch, functionName, callback) {
+    this.patches.push(
+      (BdApi.Patcher[patchType])("LastOnline", moduleToPatch, functionName, callback)
+    );
+  }
+
   start() {
+    this.usernameCreatorModuleGetter = (() => {
+      const theString = `"User Tag"`;
+      const theFilter = x2 => x2 && x2.toString?.().includes(theString); // I don't trust the BD's byString filter
+      const theFilterMain = x => x && Object.values(x).some(theFilter);
+      const theModule = BdApi.Webpack.getModule(theFilterMain);
+      // return Object.values(BdApi.Webpack.getModule(theFilterMain)).filter(theFilter)[0];
+      // return Object.keys(obj).find(prop => obj[prop].toString?.().includes(theString));
+      const funcName = Object.keys(theModule).find(prop => theFilter(theModule[prop]));
+      return { funcName, theFunc: theModule[funcName], theModule };
+    })();
     this.t = event => {
       const userId = event.updates[0].user.id;
       const status = event.updates[0].status;
@@ -31,7 +54,7 @@ class LastOnline {
           this.statuses[userIndex].newDate = new Date();
         } else {
           const user = UserStore.getUser(userId);
-      
+
           if (user) {
             this.statuses.push({
               userId,
@@ -41,17 +64,21 @@ class LastOnline {
           }
         }
       }
-      
 
       console.log(`${UserStore.getUser(event.updates[0].user.id)} has went ${status}`);
-      console.log(this.statuses)
+      console.log(this.statuses);
     };
 
     BdApi.Webpack.getModule(e => e.dispatch && !e.emitter && !e.commands).subscribe("PRESENCE_UPDATES", this.t);
+    const usernameCreatorModule = this.usernameCreatorModuleGetter;
+    this.addPatch("after", usernameCreatorModule.theModule, usernameCreatorModule.funcName, (_, args, ret) => {
+      console.log("patch worked!", ret);
+    });
   }
 
   stop() {
     BdApi.Webpack.getModule(e => e.dispatch && !e.emitter && !e.commands).unsubscribe("PRESENCE_UPDATES", this.t);
+    this.patches.forEach(x => x());
   }
 }
 
